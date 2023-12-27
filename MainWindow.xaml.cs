6039ab185a2fe8ce9c4be7ed005aca2e842f21e0
reallyShould <reallyShould@pm.me>
2023-12-28 00:28:00 +0700

    Redesign

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
+    /// https://colorhunt.co/palette/0926351b42425c83749ec8b9
 
     public partial class MainWindow : Window
     {
