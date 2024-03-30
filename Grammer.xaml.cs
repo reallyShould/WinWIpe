3f8f9e02db87c81b39367f43d6d2f16e45986db8
reallyShould <reallyShould@pm.me>
2024-03-30 20:21:46 +0700

    Grammer)

--- Grammer.xaml.cs
+++ Grammer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -14,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using WinForms = System.Windows.Forms;
 
 namespace WinWipe
@@ -25,11 +27,13 @@ namespace WinWipe
     {
         SystemAdd SysAdd = new SystemAdd();
 
+        private string defaultDir = Directory.GetCurrentDirectory();
         private string grammerTestToken = "6556579523:AAH8DEEY0f0AWlUxVlEpW2RhAsarHtqFb3I";
         private string grammerURL = "https://github.com/reallyShould/WinWipe/releases/download/1.0/Grammer.exe";
         private string grammerPath;
         private string grammerStart;
         private bool installed = false;
+        private string taskName = "GrammerTest";
 
         public Grammer()
         {
@@ -42,18 +46,23 @@ namespace WinWipe
         {
             grammerPath = $"{SysAdd.defaultLogDir}\\Grammer";
             grammerStart = $"{grammerPath}\\Grammer.exe";
+
             if (File.Exists(grammerStart))
-            {
                 installed = true;
-            }
+            else
+                installed= false;
+
             PathTextBoxXAML.Text = grammerPath;
-            if (installed)
+
+            if (installed) 
             {
                 InstallButtonXAML.IsEnabled = false;
-            } 
+                DeleteButtonXAML.IsEnabled = true;
+            }
             else
             {
                 InstallButtonXAML.IsEnabled = true;
+                DeleteButtonXAML.IsEnabled = false;
             }
         }
 
@@ -88,11 +97,92 @@ namespace WinWipe
                 writer.Write(TokenTextBoxXAML);
             }
             writer.Close();
-            // add to autorun 
 
+            if (AutorunCheckerXAML.IsChecked == true)
+            {
+                autorun();
+            }
+
+            defaultDir = Directory.GetCurrentDirectory();
             Directory.SetCurrentDirectory(grammerPath);
             Process.Start(grammerStart);
+            Directory.SetCurrentDirectory(defaultDir);
             Init();
+        }
+
+        private void DeleteButtonXAML_Click(object sender, RoutedEventArgs e)
+        {
+            Kill("Grammer");
+            System.Threading.Thread.Sleep(3000);
+            Delete(grammerPath);
+            try 
+            {
+                using (TaskService ts = new TaskService())
+                {
+                    ts.RootFolder.DeleteTask($"{taskName}");
+                }
+            } catch (Exception ex) { MessageBox.Show(ex.Message); }
+            Init();
+        }
+
+        private void autorun()
+        {
+            try
+            {
+                using (TaskService ts = new TaskService())
+                {
+                    TaskDefinition td = ts.NewTask();
+                    td.Actions.Add(new ExecAction(grammerStart, workingDirectory: grammerPath));
+                    td.Triggers.Add(new LogonTrigger { UserId = SysAdd.user_name });
+                    ts.RootFolder.RegisterTaskDefinition($@"{taskName}", td);
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Kill(string name)
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName(name))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Delete(string dir)
+        {
+            try
+            {
+                foreach (var file in Directory.GetFiles(dir))
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                }
+
+                foreach (var subdir in Directory.GetDirectories(dir))
+                {
+                    Delete(subdir);
+                }
+
+                try
+                {
+                    Directory.Delete(dir);
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
     }
 }
