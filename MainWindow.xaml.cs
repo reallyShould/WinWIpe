b5d16885148be224e435e5b230bf2319de40aed0
reallyShould <reallyShould@pm.me>
2024-04-19 17:36:06 +0700

    ?

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -206,7 +206,7 @@ namespace WinWipe
             {
                 using (var client = new WebClient())
                 {
-                    client.DownloadFile("", $"{SysAdd.defaultLogDir}\\About.html");
+                    client.DownloadFile("https://github.com/reallyShould/WinWipe/releases/download/1.1b2/About.html", $"{SysAdd.defaultLogDir}\\About.html");
                 }
                 Process.Start($"{SysAdd.defaultLogDir}\\About.html");
             } catch (Exception ex) { MessageBox.Show(ex.Message); }
